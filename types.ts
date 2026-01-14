f0bd3a021c35362adbdcf5b9f38b3546123104f4
mitrapusakasentosa <work@mitrapusakasentosa.com>
2026-01-14 19:34:06 +0700

    Update types.ts

--- types.ts
+++ types.ts
@@ -7,9 +7,9 @@ export interface TaskData {
 }
 
 export const TASK_OPTIONS = [
-  { id: 1, label: "1 pesanan", desc: "satu pesanan satu produk total satu pesanan" },
-  { id: 2, label: "2 pesanan", desc: "satu pesanan dua produk total dua pesanan" },
-  { id: 3, label: "3 pesanan", desc: "satu pesanan tiga produk total tiga pesanan" },
-  { id: 4, label: "4 Pesanan", desc: "satu pesanan empat produk total empat pesanan" },
-  { id: 5, label: "5 Pesanan", desc: "satu pesanan lima produk total lima pesanan" },
+  { id: 1, label: "1 pesanan", desc: "1 PESANAN" },
+  { id: 2, label: "2 pesanan", desc: "2 PESANAN" },
+  { id: 3, label: "3 pesanan", desc: "3 PESANAN" },
+  { id: 4, label: "4 Pesanan", desc: "4 PESANAN" },
+  { id: 5, label: "5 Pesanan", desc: "5 PESANAN" },
 ];
